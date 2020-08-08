6d6705660acede0c55961a866a405768cda259be
Luiz Felipe <luizfelipesmaciel@gmail.com>
2020-08-08 18:45:24 -0300

    Added basic http response

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 
 HOST = '0.0.0.0'
@@ -8,6 +9,7 @@ PORT = 8080
 if __name__ == '__main__':
 
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcp.bind((HOST, PORT))
     tcp.listen(1)
 
@@ -22,6 +24,20 @@ if __name__ == '__main__':
 
             if not data: break
             print('[*] {} says {}'.format(client, data))
+
+            try:
+                n = conn.sendto(bytes(''.join((
+                    'HTTP/1.1 200 OK\r\n',
+                    'Content-Type: text/html\r\n'
+                    '\r\n',
+                    'Reponse'
+                    )), 'utf-8'), 0, client)
+                print(conn)
+                print('Sent {} bytes'.format(n))
+            except Exception as e:
+                print(e)
+                tcp.close()
+                sys.exit(-1)
         
         print('[*] Ending connection with {}'.format(client))
         conn.close()
