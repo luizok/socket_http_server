45cea4d124ce78a3c72f57b59469035fb175e62a
Luiz Felipe <luizfelipesmaciel@gmail.com>
2020-08-08 21:49:45 -0300

    Returning html file with favicon

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from tcp_server import TCPServer
-from pprint import pprint
+import os
 
 
 HOST = '0.0.0.0'
@@ -15,14 +15,58 @@ class HTTPServer(TCPServer):
 
         print('[*] {} -> SERVER: {} {}'.format(client, method, path))
 
-        content = bytes(
-            '<html><header><title>PAGE</title><header><body>Site á b ó ô</body></html>',
-            'utf-8'
-        )
+        if path == '/':
+            path += 'index.html'
+
+        path = os.path.join(os.getcwd(), path[1:])
+
+        if not os.path.isfile(path):
+            return bytes(''.join((
+                'HTTP/1.1 404 Not Found\r\n',
+                '\r\n'
+            )), 'utf-8')
+
+        res = None
+        if path.endswith('html'):
+            res = self.parse_text(method, path)
+        elif path.endswith('ico'):
+            res = self.parse_binary(method, path)
+
+        return res
+
+    def parse_text(self, method, path):
+
+        content = ''
+        with open(path, 'r', encoding='utf-8') as fd:
+            content = ''.join(fd.readlines())
+
+        content = bytes(content, 'utf-8')
         header = bytes(''.join([
                 'HTTP/1.1 200 OK\r\n',
                 'Server: ScratchServer\r\n',
                 'Content-Type: text/html; charset=utf-8\r\n',
+                'Content-Length: ' + str(len(content)) + '\r\n',
+                '\r\n'
+            ]),
+            'utf-8'
+        )
+
+        res = header + content
+        return res
+
+    def parse_binary(self, method, path):
+
+        content = b''
+        with open(path, 'rb') as fd:
+            byte = fd.read(4096)
+            while byte:
+                content += byte
+                byte = fd.read(4096)
+
+        header = bytes(''.join([
+                'HTTP/1.1 200 OK\r\n',
+                'Server: ScratchServer\r\n',
+                'content-Type: image/ico\r\n',
                 'Content-Length: ' + str(len(content)) + '\r\n',
                 '\r\n'
             ]),
